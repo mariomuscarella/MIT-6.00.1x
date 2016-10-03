f17989f2959192ee6c50bae6bfec40de485276ef
Mario <mario.muscarella@gmail.com>
2016-10-03 18:38:49 -0400

    Create code.py

---
+++ Problem-Set-7/Problem2/code.py
@@ -0,0 +1,36 @@
+class Trigger(object):
+    def evaluate(self, story):
+        """
+        Returns True if an alert should be generated
+        for the given news item, or False otherwise.
+        """
+        raise NotImplementedError
+
+# Whole Word Triggers
+# Problems 2-5
+
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def isWordIn(self, text):
+        text = self.replacePunc(text)
+        return self.word in text.lower().split()
+
+    def replacePunc(self, text):
+        for p in string.punctuation:
+            text = text.replace(p, ' ')
+
+        return text
+
+class TitleTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getTitle())
+
+class SubjectTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getSubject())
+
+class SummaryTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getSummary())
